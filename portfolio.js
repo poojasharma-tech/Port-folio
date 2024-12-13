9eb006e8b71e3d6656e765baafb2db6eed40581c
poojasharma-tech <poojasharma22102001@gmail.com>
2024-12-13 20:08:26 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+// JavaScript for opening and closing the navigation overlay
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+// Smooth scrolling for navigation links
+$(document).ready(function(){
+    $('a.smoothScroll').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
